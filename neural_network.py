272af95975f68283b71ed83eaeb50daffa115e45
eladyesh <102996033+eladyesh@users.noreply.github.com>
2023-03-29 16:11:49 +0300

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -8,7 +8,7 @@ import seaborn as sns
 from sklearn.metrics import mean_squared_error as mse
 
 
-def plot_confusion_matrix(y_true, y_pred):
+def plot_confusion_matrix(cm):
     """
     This function takes in true labels and predicted labels as NumPy arrays and plots the corresponding confusion matrix using Seaborn.
 
@@ -19,9 +19,6 @@ def plot_confusion_matrix(y_true, y_pred):
     Returns:
         None
     """
-    # Generate the confusion matrix
-    cm = confusion_matrix(y_true, y_pred)
-
     # Set the figure size
     plt.figure(figsize=(8, 6))
 
@@ -48,68 +45,66 @@ df = pd.read_excel("train_website.xlsx", header=None, skiprows=1)
 pd.DataFrame(df)
 
 # Split the dataset into features and target
-X = df.iloc[:, :-1]  # all columns before the last --> data
-y = df.iloc[:, -1]  # the last column --> result
+X = np.array(df.iloc[:, :-1])  # all columns before the last --> data
+Y = np.array(df.iloc[:, -1])  # the last column --> result
+
+y1 = Y*0
+y1[np.argwhere(Y == 1)] = 1
+y2 = Y*0
+y2[np.argwhere(Y == -1)] = 1
+y_new = np.array([y1, y2]).T
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y_new, test_size=0.2, random_state=1)
 
 # Build the neural network model
-model = tf.Sequential([
- tf.layers.Dense(32, activation='relu', input_shape=(X_train.shape[1],)),
- tf.layers.Dense(16, activation='tanh'),
- tf.layers.Dense(1)
-])
-
-# Build the neural network model
-# model = tf.models.Sequential()
+model = tf.models.Sequential()
 # model.add(tf.layers.Dense(units=30, activation='relu'))  # input - 30 attributes
-# model.add(tf.layers.Dense(units=1, activation='tanh'))  # output - 1 attribute
+# model.add(tf.layers.Dense(units=2, activation='tanh'))  # output - 1 attribute
+# model.add(tf.layers.Dense(units=2, activation='softmax'))  # output - 1 attribute
 
-# Build the neural network model
-# model = tf.Sequential([
-#   tf.layers.Dense(30, activation='relu', input_shape=(X_train.shape[1],)),
-#   tf.layers.Dense(15, activation='relu'),
-# ])
+model.add(tf.layers.Dense(units=2, activation='relu'))  # output - 1 attribute
+model.add(tf.layers.Dense(units=2, activation='softmax'))  # output - 1 attribute
 
 # Compile the model
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 
 # Train the model
-history = model.fit(X_train, y_train, epochs=100, validation_split=0.2, verbose=0)
+history = model.fit(X_train, y_train, epochs=1000, validation_split=0.2, verbose=0)
 
 # Predict on the test set
 y_pred = np.round(model.predict(X_test))
 # y_pred = (y_pred > 0.5).astype(int)
+
+# Example features
+example_features = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+# example_features = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+# Reshape example features into a 2D numpy array
+example_features_array = np.array(example_features).reshape(1, -1)
+state = np.array([0, 1])
+
+# Predict the class (malicious or not) of the example URL
+prediction = model.predict(example_features_array)
+prediction = np.dot(prediction, state)[0]
+print(f"Prediction: {prediction}")
+print("=" * 32)
+if int(prediction) == 0:
+    print("The example URL is predicted to be non-malicious.")
+elif int(prediction) == 1:
+    print("The example URL is predicted to be malicious.")
+
+# Evaluate the model
+cm = confusion_matrix(np.dot(y_test, state), np.dot(y_pred, state))
+print("=" * 32)
+print('Confusion Matrix:\n', cm)
+print("=" * 32)
+plot_confusion_matrix(cm)
 
 print("=" * 32)
 print(f'Loss: {history.history["loss"][-1]}')
 loss = history.history['loss']
 plt.plot(loss)
 
-# Evaluate the model
 print("=" * 32)
 print('mse:', mse(y_test, y_pred))
-print("=" * 32)
-print('Confusion Matrix:\n', confusion_matrix(y_test, y_pred))
-print("=" * 32)
-
-# Example features
-# example_features = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0]
-example_features = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-# Reshape example features into a 2D numpy array
-example_features_array = np.array(example_features).reshape(1, -1)
-
-# Predict the class (malicious or not) of the example URL
-prediction = model.predict(example_features_array)
-print(f"Prediction: {prediction}")
-print("=" * 32)
-if prediction > 0.5:
-    print("\n\n\nThe example URL is predicted to be malicious.")
-else:
-    print("\n\n\nThe example URL is predicted to be non-malicious.")
-
-# Plot the confusion matrix using the function
-print("Confustion Matrix as seaborn")
-plot_confusion_matrix(y_test, y_pred)
