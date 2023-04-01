d55d911121067e4d9967252a5e958da74d6072f2
eladyesh <102996033+eladyesh@users.noreply.github.com>
2023-04-01 22:19:05 +0300

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -1,25 +1,24 @@
 import numpy as np
 import pandas as pd
-import tensorflow.keras as tf
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix
-import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import mean_squared_error as mse
-import random
+import tensorflow.keras as tf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, mean_squared_error as mse
+from Data import CollectData
+
+URL = 'https://web.whatsapp.com/'
 
 
 def plot_confusion_matrix(cm):
     """
     This function takes in true labels and predicted labels as NumPy arrays and plots the corresponding confusion matrix using Seaborn.
-
     Parameters:
-        y_true (numpy.ndarray): True labels represented as a NumPy array.
-        y_pred (numpy.ndarray): Predicted labels represented as a NumPy array.
-
+        cm: the confusion matrix
     Returns:
         None
     """
+
     # Set the figure size
     plt.figure(figsize=(8, 6))
 
@@ -34,32 +33,30 @@ def plot_confusion_matrix(cm):
     plt.show()
 
 
-# Define class labels
-class_names = ['Class 0', 'Class 1']
+df = pd.read_csv('Training_Dataset.csv')
+print('Got CSV')
 
-# Load the dataset
-# url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00327/Training%20Dataset.arff'
-# df = pd.read_csv(url, header=None, comment='@', sep=',')
-
-# Load the Excel file into a pandas DataFrame, ignoring headers
-df = pd.read_excel("train_website.xlsx", header=None, skiprows=1)
-pd.DataFrame(df)
+df = pd.DataFrame((df['having_IP_Address'], df['URL_Length'], df['Shortening_Service'],
+                   df['having_At_Symbol'], df['double_slash_redirecting'], df['Prefix_Suffix'],
+                   df['having_Sub_Domain'], df['SSLfinal_State'], df['Domain_registration_length'],
+                   df['Favicon'], df['HTTPS_token'], df['Request_URL'], df['URL_of_Anchor'], df['Links_in_tags'],
+                   df['Redirect'], df['on_mouseover'], df['RightClick'], df['popUpWindow'], df['Iframe'],
+                   df['age_of_domain'], df['DNSRecord'], df['Result'])).T
 
 # Split the dataset into features and target
-X = np.array(df.iloc[:, :-1])  # all columns before the last --> data
-Y = np.array(df.iloc[:, -1])  # the last column --> result
+x = np.array(df.iloc[:, :-1])  # data
+y = np.array(df.iloc[:, -1])  # result
 
 # Classify
-y1 = Y*0
-y1[np.argwhere(Y == 1)] = 1
-y2 = Y*0
-y2[np.argwhere(Y == -1)] = 1
+y1 = y * 0
+y1[np.argwhere(y == 1)] = 1
+y2 = y * 0
+y2[np.argwhere(y == -1)] = 1
 y_new = np.array([y1, y2]).T
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y_new, test_size=0.2, random_state=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y_new, test_size=0.2, random_state=1)
 
-# Build the neural network model
 model = tf.models.Sequential()
 model.add(tf.layers.Dense(units=2, activation='sigmoid'))  # output - 1 attribute
 model.add(tf.layers.Dense(units=2, activation='softmax'))  # output - 1 attribute
@@ -68,35 +65,18 @@ model.add(tf.layers.Dense(units=2, activation='softmax'))  # output - 1 attribut
 model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
 
 # Train the model
-history = model.fit(X_train, y_train, epochs=100, validation_split=0.2, verbose=0)
+history = model.fit(x_train, y_train, epochs=100, validation_split=0.2, verbose=0)
+
+example_features = CollectData()[URL]
+if example_features is None:
+    quit(print('DATA EXTRACTION FAILED'))
 
 # Predict on the test set
-y_pred = np.round(model.predict(X_test))
-# y_pred = (y_pred > 0.5).astype(int)
-
-# Example features
-# prediction --> 7.475396728295891e-07
-# example_features = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-
-# until we enter the real list
-example_features = [random.randint(-1, 1) for i in range(30)]
+y_pred = np.round(model.predict(x_test))
 
 # Reshape example features into a 2D numpy array
 example_features_array = np.array(example_features).reshape(1, -1)
 state = np.array([0, 1])
-
-# Predict the class (malicious or not) of the example URL
-prediction = model.predict(example_features_array)
-prediction = np.dot(prediction, state)[0]
-print(f"Prediction: {prediction}")
-if prediction > 0.5:
-    print("The Features List: ", example_features)
-    # example phishing --> [-1, 0, 1, -1, 0, -1, 0, 0, 0, 1, 1, 1, 1, -1, 1, 1, 1, -1, -1, -1, -1, 1, 0, 0, 0, 1, -1,
-    # 1, 0, 1]
-    print("The example URL is predicted to be malicious.")
-else:
-    print("The Features List: ", example_features)
-    print("The example URL is predicted to be non-malicious.")
 
 # Evaluate the model
 cm = confusion_matrix(np.dot(y_test, state), np.dot(y_pred, state))
@@ -112,3 +92,16 @@ print('MSE:', mse(y_test, y_pred))
 # Plot
 plt.plot(loss)
 plot_confusion_matrix(cm)
+
+# Predict the class (malicious or not) of the example URL
+prediction = model.predict(example_features_array)
+prediction = np.dot(prediction, state)[0]
+print(f"Prediction: {prediction}")
+if prediction > 0.5:
+    print("The Features List: ", example_features)
+    # example phishing --> [-1, 0, 1, -1, 0, -1, 0, 0, 0, 1, 1, 1, 1, -1, 1, 1, 1, -1, -1, -1, -1, 1, 0, 0, 0, 1, -1,
+    # 1, 0, 1]
+    print("The example URL is predicted to be malicious.")
+else:
+    print("The Features List: ", example_features)
+    print("The example URL is predicted to be non-malicious.")
