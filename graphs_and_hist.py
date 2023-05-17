e966f822296d18b7adf4ab35733896121ae7a38c
eladyesh <102996033+eladyesh@users.noreply.github.com>
2023-05-17 19:57:43 +0300

    Update graphs_and_hist.py

--- graphs_and_hist.py
+++ graphs_and_hist.py
@@ -12,21 +12,24 @@ df = pd.DataFrame((df['having_IP_Address'], df['URL_Length'], df['Shortening_Ser
                    df['age_of_domain'], df['DNSRecord'], df['Result'])).T
 
 # Loop through each column (excluding "Result") and plot a graph
-# Loop through each column (excluding "Result") and plot a graph
 for col in df.columns[:-1]:
-    plt.plot(df[col], df["Result"], linestyle="-")
-    plt.xlabel(col)
-    plt.ylabel("Result")
-    plt.show()
-    
+    if col not in ['having_Sub_Domain', 'Shortening_Service']:
+        plt.scatter(df[col], df["Result"])
+        plt.xlabel(col)
+        plt.ylabel("Result")
+        plt.title(f"{col} vs. Result")
+        plt.show()
+
 # Plot a histogram of the "Result" column
 plt.hist(df["Result"])
 plt.xlabel("Result")
 plt.ylabel("Frequency")
+plt.title("Distribution of Result")
 plt.show()
 
 # Plot a histogram of the "URL_Length" column
 plt.hist(df["URL_Length"])
 plt.xlabel("URL_Length")
 plt.ylabel("Frequency")
+plt.title("Distribution of URL Length")
 plt.show()
